d650df0c27c44b29f0e694e1243701ed9d198f2e
Dorothy <jhu4@wpi.edu>
2017-05-16 12:07:54 -0400

    Update 461HammingDistance.java

--- week1/461HammingDistance.java
+++ week1/461HammingDistance.java
@@ -1,5 +1,5 @@
-//easy
-public int hammingDistance(int x, int y) {
+//My initial solution
+public int hammingDistance1(int x, int y) {
     int xor=x^y;
     int result=0;
     while(xor!=0){
@@ -7,6 +7,17 @@ public int hammingDistance(int x, int y) {
             result++;
         }
         xor=xor>>1;
+    }
+    return result;
+}
+
+//Better solution
+public int hammingDistance2(int x, int y) {
+    int xor=x^y;
+    int result=0;
+    while(xor!=0){
+        result++;
+        xor &= (xor-1); // this operation removes the right most 1 in binary form
     }
     return result;
 }

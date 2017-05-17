e75cb9fd87d67f0bbea7a7e1c5b63f70b1fa7d09
Dorothy <jhu4@wpi.edu>
2017-05-17 11:36:20 -0400

    Create 9PalindromNumber.java

---
+++ week1/9PalindromNumber.java
@@ -0,0 +1,24 @@
+public class Solution {
+    //My solution
+    public boolean isPalindrome1(int x) {
+        if(x<0) return false;
+        int xx=x;
+        int y=0;
+        while(x!=0){
+            y=y*10+x%10;
+            x/=10;
+        }
+        return y==xx;
+    }
+    
+    //Another way(other's solution) to do it is to only compare half of the digits
+    public boolean isPalindrome2(int x) {
+      if (x<0 || (x!=0 && x%10==0)) return false;
+      int rev = 0;
+      while (x>rev){
+        rev = rev*10 + x%10;
+        x = x/10;
+      }
+      return (x==rev || x==rev/10);
+    }
+}

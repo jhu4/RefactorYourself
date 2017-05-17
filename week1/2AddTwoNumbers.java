407c88eaa84144cb77f43c36d92ca649fcf48b16
Dorothy <jhu4@wpi.edu>
2017-05-16 22:09:55 -0400

    Update 2AddTwoNumbers.java

--- week1/2AddTwoNumbers.java
+++ week1/2AddTwoNumbers.java
@@ -29,7 +29,7 @@ public class AddTwoNumbersSolution {
     }
     
     //Others code that is more consice
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
         ListNode c1 = l1;
         ListNode c2 = l2;
         ListNode sentinel = new ListNode(0);
@@ -51,5 +51,24 @@ public class AddTwoNumbersSolution {
         if (sum / 10 == 1)
             d.next = new ListNode(1);
         return sentinel.next;
+    }
+    
+    //Another answer that I also appreciate
+    public ListNode addTwoNumbers3(ListNode l1, ListNode l2) {
+        ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            ListNode cur = new ListNode(0);
+            int sum = ((l2 == null) ? 0 : l2.val) + ((l1 == null) ? 0 : l1.val) + carry;
+            cur.val = sum % 10;
+            carry = sum / 10;
+            prev.next = cur;
+            prev = cur;
+            
+            l1 = (l1 == null) ? l1 : l1.next;
+            l2 = (l2 == null) ? l2 : l2.next;
+        }
+        return head.next;
     }
 }

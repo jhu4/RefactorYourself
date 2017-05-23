b5793e2657584b946c9eeaf47364739821c4b5e0
Dorothy <jhu4@wpi.edu>
2017-05-23 15:33:27 -0400

    Update 28ImplementStrStr.java

--- week2/28ImplementStrStr.java
+++ week2/28ImplementStrStr.java
@@ -2,27 +2,25 @@ public class ImplementStrStrSolution {
     
     //holly crap! I fucking could use substring instead
     public int strStr2(String haystack, String needle) {
-        int l1 = haystack.length(), l2 = needle.length();
-        if (l1 < l2) {
-            return -1;
-        } else if (l2 == 0) {
-            return 0;
-        }
-        int threshold = l1 - l2;
-        for (int i = 0; i <= threshold; ++i) {
-            if (haystack.substring(i,i+l2).equals(needle)) {
+        if(haystack==null || needle==null) return -1;
+        if(needle.length()==0) return 0; //wierd edge case
+        
+        for(int i=0;i<(haystack.length()-needle.length()+1);i++){
+            if(haystack.substring(i,i+needle.length()).equals(needle)){
                 return i;
             }
         }
         return -1;
     }
     
+    
+    //My first stupid answer
     public int strStr1(String haystack, String needle) {
         if(haystack==null || needle==null) return -1;
         if(needle.length()==0) return 0; //wierd edge case
         
         for(int i=0;i<(haystack.length()-needle.length()+1);i++){
-            if(haystack.charAt(i)==needle.charAt(0)){
+            if(haystack.charAt(i)==needle.charAt(0)){   //DO NOT USE THIS STUPID FOR LOOP METHOD TO COMPARE STRINGS AGAIN
                 int j=1;
                 while(j<needle.length() && haystack.charAt(i+j)==needle.charAt(j)){
                     j++;

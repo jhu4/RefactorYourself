d8705f2b213585d4836d51651570a09aa2bd1479
Dorothy <jhu4@wpi.edu>
2017-05-23 15:29:42 -0400

    Update 28ImplementStrStr.java

--- week2/28ImplementStrStr.java
+++ week2/28ImplementStrStr.java
@@ -1,5 +1,23 @@
 public class ImplementStrStrSolution {
-    public int strStr(String haystack, String needle) {
+    
+    //holly crap! I fucking could use substring instead
+    public int strStr2(String haystack, String needle) {
+        int l1 = haystack.length(), l2 = needle.length();
+        if (l1 < l2) {
+            return -1;
+        } else if (l2 == 0) {
+            return 0;
+        }
+        int threshold = l1 - l2;
+        for (int i = 0; i <= threshold; ++i) {
+            if (haystack.substring(i,i+l2).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public int strStr1(String haystack, String needle) {
         if(haystack==null || needle==null) return -1;
         if(needle.length()==0) return 0; //wierd edge case
         
@@ -14,4 +32,5 @@ public class ImplementStrStrSolution {
         }
         return -1;
     }
+ 
 }

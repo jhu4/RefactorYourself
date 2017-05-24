367f631ea847934b188bbf5c7b025df1ab587ddc
Dorothy <jhu4@wpi.edu>
2017-05-23 22:32:11 -0400

    Update 17LetterCombinationsOfAPhoneNumber.java

--- week2/17LetterCombinationsOfAPhoneNumber.java
+++ week2/17LetterCombinationsOfAPhoneNumber.java
@@ -23,7 +23,7 @@ public class LetterCombinationsOfAPhoneNumberSolution {
     }
     
     //Solution using Queue from https://discuss.leetcode.com/topic/8465/my-java-solution-with-fifo-queue
-    public List<String> letterCombinations(String digits) {
+    public List<String> letterCombinations1(String digits) {
         LinkedList<String> ans = new LinkedList<String>();
         if(digits==null || digits.length()==0) return ans;
         String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};

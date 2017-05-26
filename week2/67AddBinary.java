d57a8135818a215ff4f4373f6cb5e3902c209cc2
Dorothy <jhu4@wpi.edu>
2017-05-26 14:12:22 -0400

    Update 67AddBinary.java

--- week2/67AddBinary.java
+++ week2/67AddBinary.java
@@ -35,4 +35,21 @@ public class Solution {
         if(carry=='1') return "1"+aa.toString();
         else return aa.toString();
     }
+    
+    //Another solution that is pretty concise
+    public String addBinary2(String a, String b) {
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        int i = a.length() - 1, j = b.length() - 1;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
 }

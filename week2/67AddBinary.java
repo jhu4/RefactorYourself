bb7fd2636f65cab357f412e0fd394bd611341e18
Dorothy <jhu4@wpi.edu>
2017-05-26 14:06:48 -0400

    Create 67AddBinary.java

---
+++ week2/67AddBinary.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public String addBinary(String a, String b) {
+        if(a.length()<b.length()){ //swapp a b is a is shorter
+            String temp=a;
+            a=b;
+            b=temp;
+        }
+        
+        StringBuilder aa=new StringBuilder(a);
+        int alen=a.length(),blen=b.length();
+        char carry='0';
+        for(int i=0;i<alen;i++){
+            char achar=a.charAt(alen-1-i); //from back to front
+            char bchar=i<blen?b.charAt(blen-1-i):'0';
+            
+            //manipulate the digits in string buidler
+            switch(achar+bchar+carry){
+                case ('1'+'1'+'1'):
+                    aa.setCharAt(alen-1-i,'1');
+                    //carry is still one, we don't need to set it
+                    break;
+                case ('1'+'1'+'0'):
+                    aa.setCharAt(alen-1-i,'0');
+                    carry='1';
+                    break;
+                case ('1'+'0'+'0'):
+                    aa.setCharAt(alen-1-i,'1');
+                    carry='0';
+                    break;
+                case ('0'+'0'+'0'):
+                    //nothing happened
+                    break;
+            }
+        }
+        if(carry=='1') return "1"+aa.toString();
+        else return aa.toString();
+    }
+}

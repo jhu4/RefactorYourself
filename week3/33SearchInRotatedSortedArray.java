07340af1f8d8c5679f1046be702f7d6b7204303c
Dorothy <jhu4@wpi.edu>
2017-05-31 12:31:25 -0400

    Update 33SearchInRotatedSortedArray.java

--- week3/33SearchInRotatedSortedArray.java
+++ week3/33SearchInRotatedSortedArray.java
@@ -1,4 +1,30 @@
 public class SearchInRotatedSortedArraySolution {
+    public int search2(int[] nums, int target) {
+        if(nums==null || nums.length==0) return -1;
+        
+        int lo=0,hi=nums.length-1,mid=(lo+hi)/2;
+        
+        while(lo<=hi){
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>=nums[lo]){//case 1: where the mid is before the pivot rotate point
+                                    //nums[mid]>nums[hi] or nums[mid]>=nums[lo] worked but num[mid]>nums[lo] didn't work
+                                    //since we have to consider the case where nums[lo]=nums[mid], a case where mid is before the pivot
+                                    //but we don't need to consider the edge case where nums[mid]=nums[hi] because that is the last step
+                                    //of the entire binary search
+                                    
+                if(target>=nums[lo]&&target<nums[mid]) hi=mid-1;
+                else lo=mid+1;
+            }
+            else{ //case 2: where the mid is after the pivot 
+                if(target>nums[mid]&&target<=nums[hi]) lo=mid+1;
+                else hi=mid-1;
+            }
+            mid=(lo+hi)/2;
+        }
+        
+        return -1;
+    }
+    
     //My initial solution... ran slowly
     public int search(int[] nums, int target) {
         if(nums==null||nums.length==0) return -1;

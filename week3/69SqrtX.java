64ecaaa43caca08845ec39145ef43ca6a85bfb7a
Dorothy <jhu4@wpi.edu>
2017-05-31 13:57:02 -0400

    Update 69SqrtX.java

--- week3/69SqrtX.java
+++ week3/69SqrtX.java
@@ -1,17 +1,25 @@
-public class Solution {
-    public int mySqrt(int x) {
+public class SqrtXSolution {
+    //Newton method
+    public int mySqrt2(int x) {
+        long r = x;
+        while (r*r > x)
+            r = (r + x/r) / 2;
+        return (int) r;
+    }
+    
+    //My initial solution: binary search
+    public int mySqrt1(int x) {
         if(x==0) return 0;
         if(x==1) return 1;
         
-        int lo=1,hi=x/2,mid=(int)(lo/2.0+hi/2.0); //lo starts at 1 to avoid divide by zero exception
+        int lo=1,hi=x/2,mid=(lo+hi)/2;
         
         while(lo<=hi){
             if(x/mid==mid||(x/(mid+1)<(mid+1)&&x/mid>mid)) return mid; //using x/mid==mid instead of x==mid*mid to avoid overflow
             else if(x/mid<mid) hi=mid-1;
             else lo=mid+1;
-            mid=(int)(lo/2.0+hi/2.0); //using double division to avoid lossing accuracy and avoid overflow
+            mid=(lo+hi)/2; //we don't need to consider the overflow case because lo+hi is at max 2*hi where hi is at max x/2
         }
-        
         return lo;
     }
 }

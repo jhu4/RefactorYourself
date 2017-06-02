6ffb91a10376f40d64d510d67d636699bd7861d6
Dorothy <jhu4@wpi.edu>
2017-06-02 18:43:18 -0400

    Update 205IsomorphicStrings.java

--- week3/205IsomorphicStrings.java
+++ week3/205IsomorphicStrings.java
@@ -20,4 +20,14 @@ public class IsomorphicStringsSolution {
         }
         return true;
     }
+    
+    //Shorter solution using only one int[]
+    public boolean isIsomorphic2(String s1, String s2) {
+        int[] m = new int[512];
+        for (int i = 0; i < s1.length(); i++) {
+            if (m[s1.charAt(i)] != m[s2.charAt(i)+256]) return false;
+            m[s1.charAt(i)] = m[s2.charAt(i)+256] = i+1;
+        }
+        return true;
+    }
 }

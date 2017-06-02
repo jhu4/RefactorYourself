c6b67b33756b2d73116574be66b167504f2de914
Dorothy <jhu4@wpi.edu>
2017-06-02 15:39:26 -0400

    Create 39CombinationSum.java

---
+++ week3/39CombinationSum.java
@@ -0,0 +1,21 @@
+public class CombinationSumSolution {
+    //Recursive solution
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list=new ArrayList<>();
+        Arrays.sort(candidates);
+        combinationSum(list,new ArrayList<>(),candidates,0,target);
+        return list;
+    }
+    
+    private void combinationSum(List<List<Integer>> list, List<Integer> array, int[] candidates, int start, int left){
+        if(left<0) return;
+        else if(left==0) list.add(new ArrayList<Integer>(array));
+        else{
+            for(int i=start;i<candidates.length && left>=candidates[i];i++){ //left>=candidates[i] -> pruning the recusion tree
+                array.add(candidates[i]);
+                combinationSum(list,array,candidates,i,left-candidates[i]);
+                array.remove(array.size()-1);
+            }
+        }
+    }
+}

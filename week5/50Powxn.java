45a8e8074a1309113b1a03c5b9a7a80b9ad26248
Dorothy <jhu4@wpi.edu>
2017-06-13 17:08:41 -0400

    Create 50Powxn.java

--- week5/50Powxn.java
+++ week5/50Powxn.java
@@ -1,10 +1,11 @@
 public class PowxnSolution {
+    //Divide and conquer solution
     public double myPow(double x, int n) {
         if(n==0) return 1;
         double temp=myPow(x,n/2);
         
         if(n%2==0) return temp*temp;
-        else if(n<0) return temp*temp/x;
+        else if(n<0) return temp*temp/x; //if n is negative, then the multiplier is 1/x
         else return temp*temp*x;
     }
 }

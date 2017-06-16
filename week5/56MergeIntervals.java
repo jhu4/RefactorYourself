c847c5ec9fe13bbbfa7a4a3b218b914bd4394d7a
Dorothy <jhu4@wpi.edu>
2017-06-16 15:47:57 -0400

    Update 56MergeIntervals.java

--- week5/56MergeIntervals.java
+++ week5/56MergeIntervals.java
@@ -8,6 +8,33 @@
  * }
  */
 public class Solution {
+    //Other's solution. Same idea but optimize some operations
+    public List<Interval> merge3(List<Interval> intervals) {
+        if (intervals.size() <= 1)
+            return intervals;
+        
+        // Sort by ascending starting point using an anonymous Comparator
+        intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
+        
+        List<Interval> result = new LinkedList<Interval>();
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
+        
+        for (Interval interval : intervals) {
+            if (interval.start <= end) // Overlapping intervals, move the end if needed
+                end = Math.max(end, interval.end);
+            else {                     // Disjoint intervals, add the previous one and reset bounds
+                result.add(new Interval(start, end));
+                start = interval.start;
+                end = interval.end;
+            }
+        }
+        
+        // Add the last interval
+        result.add(new Interval(start, end));
+        return result;
+    }
+    
     //it turn out merge1 TLE becasue I used list.remove which is O(n) operation
     //Same idea, instead of manipulating in the original list we add the qualified one to the new list
     public List<Interval> merge2(List<Interval> intervals) {

d1773322f5661244d0a8973225fa295fe83d77c5
Dorothy <jhu4@wpi.edu>
2017-06-26 15:54:55 -0400

    Create 75SortColors.java

---
+++ week7/75SortColors.java
@@ -0,0 +1,19 @@
+public class SortColorsSolution {
+    //My solution based on counting sort
+    public void sortColors(int[] nums) {
+        if(nums == null || nums.length <= 1) return;
+        
+        int[] counting = new int[3];
+        for(int i : nums) {
+            counting[i]++;
+        }
+        
+        int index = 0;
+        for(int i = 0; i < 3; i++) {
+            while(counting[i]>0) {
+                nums[index++] = i;
+                counting[i]--;
+            }
+        }
+    }
+}

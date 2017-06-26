33c3528cb752cbc67d9c8b55de8513fe44d8d839
Dorothy <jhu4@wpi.edu>
2017-06-26 17:09:52 -0400

    Create 347TopKFrequentElements.java

---
+++ week7/347TopKFrequentElements.java
@@ -0,0 +1,27 @@
+public class TopKFrequentElementsSolution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> res = new ArrayList<>();
+        List<Integer>[] bucket = new List[nums.length + 1]; //we use a list because they might be more than one elements having
+        Map<Integer, Integer> freq = new HashMap<>();   //having the same frequency
+        
+        for(int i : nums) {
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        }
+        
+        for(int key : freq.keySet()) {
+            int frequency = freq.get(key);
+            if(bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+        
+        for(int i = bucket.length - 1; i >= 0 && res.size() < k; i--) {
+            if(bucket[i] != null) {
+                res.addAll(bucket[i]);
+            }
+        }
+        
+        return res;
+    }
+}

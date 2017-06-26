5305d68f374e806843b2f6c213e8fb948c163421
Dorothy <jhu4@wpi.edu>
2017-06-26 16:07:31 -0400

    Create SortColorsTwo.java

---
+++ week7/SortColorsTwo.java
@@ -0,0 +1,23 @@
+class SortColorsTwoSolution {
+    /**
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */
+    public void sortColors2(int[] colors, int k) {
+        if(colors == null || colors.length <= 1) return;
+        
+        int[] numberCount = new int[k+1];
+        for(int i : colors) {
+            numberCount[i]++;
+        }
+        
+        int index = 0;
+        for(int i = 0; i < numberCount.length; i++) {
+            while(numberCount[i] > 0) {
+                colors[index++] = i;
+                numberCount[i]--;
+            }
+        }
+    }
+}

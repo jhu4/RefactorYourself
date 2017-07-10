76d0028f043d5150f9b516a3d5a73634d618dac1
Dorothy <jhu4@wpi.edu>
2017-07-09 23:36:42 -0400

    Create SymmetricBinaryTree.java

---
+++ week8/SymmetricBinaryTree.java
@@ -0,0 +1,18 @@
+public boolean isSymmetric(TreeNode root) {
+  if(root == null) {
+    return true;
+  }
+  return isSymmetric(root.left, root.right);
+}
+
+private boolean isSymmetric(TreeNode left, TreeNode right) {
+  if(left == null && right == null) {
+    return true;
+  }
+  if(left == null || right == null) {
+    return false;
+  }
+
+  boolean topValueEqual = left.key == right.key; //don't forget this step!!!
+  return topValueEqual && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+}

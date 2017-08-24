6474f8668ce5dbcb0aa0e952c3ad520942accb57
Dorothy <jhu4@wpi.edu>
2017-08-23 21:49:49 -0400

    Update LongestSubstringWithoutRepeatingCharacters.java

--- week13/LongestSubstringWithoutRepeatingCharacters.java
+++ week13/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,1 +1,20 @@
+public int longest(String input) {
+  Map<Character, Integer> map = new HashMap<>();
+  int startIndex = 0;
+  int max = Integer.MIN_VALUE;
 
+  for (int i = 0; i < input.length(); i++) {
+    Integer index = map.get(input.charAt(i));
+
+
+    if (index != null && index >= startIndex) { //if the character is in the result substring
+      startIndex = index + 1; //we update the new start index and exclude the previous character
+    } else {
+      max = Math.max(max, i + 1 - startIndex); 
+    }
+
+    map.put(input.charAt(i), i);
+  }
+
+  return max;
+}
